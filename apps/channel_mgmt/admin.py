5ce1e3bbfd57d585aca5f88cc90391f1c2e7e558
umaru <15875339926@139.com>
2024-05-29 19:05:23 +0800

    优化UI

--- apps/channel_mgmt/admin.py
+++ apps/channel_mgmt/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from django_ace import AceWidget
 from django_yaml_field import YAMLField
 from unfold.admin import ModelAdmin
@@ -32,9 +34,15 @@ class ChannelUserGroupAdmin(ModelAdmin):
 
 @admin.register(ChannelUser)
 class ChannelUserAdmin(ModelAdmin):
-    list_display = ['channel_user_group', 'user_id', 'name']
+    list_display = ['channel_user_group_link', 'user_id', 'name']
     search_fields = ['name']
     list_filter = ['name']
-    list_display_links = ['name']
+    list_display_links = ['channel_user_group_link', 'user_id']
     ordering = ['id']
     filter_horizontal = []
+
+    def channel_user_group_link(self, obj):
+        link = reverse("admin:channel_mgmt_channelusergroup_change", args=[obj.channel_user_group.id])
+        return format_html('<a href="{}">{}</a>', link, obj.channel_user_group)
+
+    channel_user_group_link.short_description = '用户组'
