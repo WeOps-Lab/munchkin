c0d02da5ba374e0933b2e248bec7d82f1be526f1
zhmf7408 <kido@canway.net>
2024-08-21 15:34:23 +0800

    feature init

--- apps/new_app/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import re_path
-
-from apps.new_app import views
-
-urlpatterns = (re_path(r"^test/$", views.test),)
--- apps/new_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-from apps.core.decorators.api_perminssion import HasRole
-
-
-@HasRole(roles=["admin"])
-def test(request):
-    return JsonResponse({"result": True})
